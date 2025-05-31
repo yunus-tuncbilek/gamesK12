6dd4be477c572a1d1d7c2591bcbb3fe4445b5ae8
ynst <yunustuncbilek@hotmail.com>
2025-05-31 13:41:09 -0700

    moved the terminal if statements outside loop

--- guess.py
+++ guess.py
@@ -23,11 +23,11 @@ for guessesTaken in range(6):
     if guess == number:
         break
 
-    if guess == number:
-        guessesTaken = str(guessesTaken + 1)
-        print('Good job, ' + myName + '! You guessed my number in ' +
-          guessesTaken + ' guesses!')
+if guess == number:
+    guessesTaken = str(guessesTaken + 1)
+    print('Good job, ' + myName + '! You guessed my number in ' +
+        guessesTaken + ' guesses!')
 
-    if guess != number:
-        number = str(number)
-        print('Nope. The number I was thinking of was ' + number + '.')
+if guess != number:
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number + '.')
